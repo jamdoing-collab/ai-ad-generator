33814cc30b3746d8c06c9e09ea62763e6d5895f4
jam <jam@jamdeMacBook-Air.local>
2026-05-08 11:32:30 +0800

    simplify password validation

--- server/validators/password.js
+++ server/validators/password.js
@@ -5,9 +5,6 @@ function validatePassword(password) {
   if (password.length < 6 || password.length > 64) {
     return '密码长度需在6-64个字符之间';
   }
-  if (!/(?=.*[a-zA-Z])(?=.*\d)/.test(password)) {
-    return '密码需同时包含字母和数字';
-  }
   return '';
 }
 
